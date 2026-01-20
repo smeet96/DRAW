41eeccfde3d0d37f60d164ef44c26030450d62ea
smeet96 <smeetsherki@gmail.com>
2026-01-20 14:28:48 +0530

    added_middleware

---
+++ apps/http-server/src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import jwt,{ verify } from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/common-backend/config";
+
+export const middleware = (req:Request,res:Response,next:NextFunction) => {
+    const token = req.headers['authorization']
+    if(!token || !JWT_SECRET){
+        return
+    }
+
+    try {
+        const decoded = jwt.verify(token,JWT_SECRET)
+        if(decoded){
+            //@ts-ignore
+            req.id = decoded.userId
+            next()
+        } 
+    }catch (error) {
+         res.status(401).json({
+            message : 'auth failed',
+            error:error
+        })
+    }
+    }
